7ab453250a9798ece678d130fdb638f7ce7f2004
Sincal <sincaljulio200@gmail.com>
2024-03-24 22:30:30 -0600

    Lógica de Guardado

--- CajeroCrud/src/app/components/transaccion/transaccion.component.ts
+++ CajeroCrud/src/app/components/transaccion/transaccion.component.ts
@@ -39,9 +39,7 @@ export class TransaccionComponent {
     });
   }
 
-
-
-  save(){
+  saveDeposito(){
     this.list();
     let maxId = Math.max(...this.listTransacciones.map(t => t.transaccionId));
     this.formTransacciones.controls['transaccionId'].setValue(maxId + 1);
@@ -58,6 +56,7 @@ export class TransaccionComponent {
       }
     });
     this.list();
+    this.formTransacciones.reset();
   }
 
   saveRetiro(){
@@ -77,7 +76,47 @@ export class TransaccionComponent {
       }
     });
     this.list();
+    this.formTransacciones.reset();
   }
+
+  saveDepositoTercero(){
+    this.list();
+    let maxId = Math.max(...this.listTransacciones.map(t => t.transaccionId));
+    this.formTransacciones.controls['transaccionId'].setValue(maxId + 1);
+    this.formTransacciones.controls['fecha'].setValue(new Date().toISOString());
+    this.formTransacciones.controls['tipo'].setValue('DepositoTercero');
+    this.formTransacciones.controls['numeroDepositoID'].setValue(0);
+    this.formTransacciones.controls['cajeroID'].setValue(1);
+    this.transaccionService.saveTransacciones(this.formTransacciones.value).subscribe(resp =>{
+    this.list();
+      if(resp){
+        this.list();
+        this.formTransacciones.reset();
+      }
+    });
+    this.list();
+    this.formTransacciones.reset();
+  }
+
+  saveDepositoCelular(){
+    this.list();
+    let maxId = Math.max(...this.listTransacciones.map(t => t.transaccionId));
+    this.formTransacciones.controls['transaccionId'].setValue(maxId + 1);
+    this.formTransacciones.controls['fecha'].setValue(new Date().toISOString());
+    this.formTransacciones.controls['tipo'].setValue('DepositoCelular');
+    this.formTransacciones.controls['cuentaDestino'].setValue(0);
+    this.formTransacciones.controls['cajeroID'].setValue(1);
+    this.transaccionService.saveTransacciones(this.formTransacciones.value).subscribe(resp =>{
+    this.list();
+      if(resp){
+        this.list();
+        this.formTransacciones.reset();
+      }
+    });
+    this.list();
+    this.formTransacciones.reset();
+  }
+
 
   newTransaccion(){
     this.formTransacciones.reset();
